85f06ee05e252a7599a7970a69b20c3356e2ab1d
astyanax <681415+astyanax@users.noreply.github.com>
2026-05-30 13:46:19 +0000

    [skip ci] style: auto-fix imports ordering and formatting

--- xna/trunk/Xenocide.MonoGame/Source/Xenocide.cs
+++ xna/trunk/Xenocide.MonoGame/Source/Xenocide.cs
@@ -31,6 +31,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Resources;
 
 using AudioSystem;
 
@@ -52,8 +53,6 @@ using ProjectXenocide.UI.Screens;
 using ProjectXenocide.Utils;
 
 using Xenocide.Resources;
-
-using System.Resources;
 
 #endregion
 
