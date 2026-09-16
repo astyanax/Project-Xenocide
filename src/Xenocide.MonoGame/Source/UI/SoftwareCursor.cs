50b65041fa6ad82de6cb0d37589b5885abd73d6e
astyanax <681415+astyanax@users.noreply.github.com>
2026-09-16 20:55:37 +0000

    [skip ci] style: auto-fix imports ordering and formatting

--- src/Xenocide.MonoGame/Source/UI/SoftwareCursor.cs
+++ src/Xenocide.MonoGame/Source/UI/SoftwareCursor.cs
@@ -1,9 +1,9 @@
+using Gum.Forms;
+using Gum.Forms.Controls;
+
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-
-using Gum.Forms;
-using Gum.Forms.Controls;
 
 using MonoGameGum;
 
